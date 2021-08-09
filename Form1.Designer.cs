6b88430e891a669672523aa3c8ee7bf99d8ea970
konry-ru <konry-ru@yandex.ru>
2021-08-09 11:40:29 +0700

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿
+namespace WinFormsPomodoro
+{
+    partial class FormTasksList
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTasksList));
+            this.listBoxTasksList = new System.Windows.Forms.ListBox();
+            this.LabelTaskList = new System.Windows.Forms.Label();
+            this.buttonEdit = new System.Windows.Forms.Button();
+            this.buttonRun = new System.Windows.Forms.Button();
+            this.buttonDeleteTask = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // listBoxTasksList
+            // 
+            this.listBoxTasksList.FormattingEnabled = true;
+            this.listBoxTasksList.ItemHeight = 18;
+            this.listBoxTasksList.Location = new System.Drawing.Point(12, 34);
+            this.listBoxTasksList.Name = "listBoxTasksList";
+            this.listBoxTasksList.Size = new System.Drawing.Size(383, 490);
+            this.listBoxTasksList.TabIndex = 0;
+            this.listBoxTasksList.SelectedIndexChanged += new System.EventHandler(this.listBoxTasksList_SelectedIndexChanged);
+            // 
+            // LabelTaskList
+            // 
+            this.LabelTaskList.AutoSize = true;
+            this.LabelTaskList.Location = new System.Drawing.Point(12, 12);
+            this.LabelTaskList.Margin = new System.Windows.Forms.Padding(3);
+            this.LabelTaskList.Name = "LabelTaskList";
+            this.LabelTaskList.Size = new System.Drawing.Size(99, 19);
+            this.LabelTaskList.TabIndex = 1;
+            this.LabelTaskList.Text = "Список задач";
+            // 
+            // buttonEdit
+            // 
+            this.buttonEdit.Location = new System.Drawing.Point(420, 395);
+            this.buttonEdit.Name = "buttonEdit";
+            this.buttonEdit.Size = new System.Drawing.Size(166, 39);
+            this.buttonEdit.TabIndex = 2;
+            this.buttonEdit.Text = "Редактировать";
+            this.buttonEdit.UseVisualStyleBackColor = true;
+            // 
+            // buttonRun
+            // 
+            this.buttonRun.Location = new System.Drawing.Point(420, 440);
+            this.buttonRun.Name = "buttonRun";
+            this.buttonRun.Size = new System.Drawing.Size(166, 39);
+            this.buttonRun.TabIndex = 3;
+            this.buttonRun.Text = "Выполнить";
+            this.buttonRun.UseVisualStyleBackColor = true;
+            // 
+            // buttonDeleteTask
+            // 
+            this.buttonDeleteTask.Location = new System.Drawing.Point(420, 485);
+            this.buttonDeleteTask.Name = "buttonDeleteTask";
+            this.buttonDeleteTask.Size = new System.Drawing.Size(166, 39);
+            this.buttonDeleteTask.TabIndex = 4;
+            this.buttonDeleteTask.Text = "Удалить";
+            this.buttonDeleteTask.UseVisualStyleBackColor = true;
+            this.buttonDeleteTask.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // FormTasksList
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 18F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.ClientSize = new System.Drawing.Size(606, 540);
+            this.Controls.Add(this.buttonDeleteTask);
+            this.Controls.Add(this.buttonRun);
+            this.Controls.Add(this.buttonEdit);
+            this.Controls.Add(this.LabelTaskList);
+            this.Controls.Add(this.listBoxTasksList);
+            this.Font = new System.Drawing.Font("Roboto", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(2, 3, 2, 3);
+            this.Name = "FormTasksList";
+            this.Text = "Pomodoro";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBoxTasksList;
+        private System.Windows.Forms.Label LabelTaskList;
+        private System.Windows.Forms.Button buttonEdit;
+        private System.Windows.Forms.Button buttonRun;
+        private System.Windows.Forms.Button buttonDeleteTask;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WinFormsPomodoro
+{
+    public partial class FormTasksList : Form
+    {
+        public FormTasksList()
+        {
+            InitializeComponent();
+            listBoxTasksList.Items.Add("help me");
+            listBoxTasksList.Items.Add("i have done this self");
+        }
+
+        private void listBoxTasksList_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
