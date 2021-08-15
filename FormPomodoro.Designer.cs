031d444f70004ab2d81ee65ea22b867d1978c5f1
konry-ru <konry-ru@yandex.ru>
2021-08-15 13:00:48 +0700

    Реализация помодоро через timer

--- FormPomodoro.Designer.cs
+++ FormPomodoro.Designer.cs
@@ -29,17 +29,23 @@ namespace WinFormsPomodoro
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormPomodoro));
             this.label1 = new System.Windows.Forms.Label();
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("PT Sans", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
-            this.label1.Location = new System.Drawing.Point(252, 40);
+            this.label1.Location = new System.Drawing.Point(242, 9);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(53, 20);
             this.label1.TabIndex = 0;
@@ -49,16 +55,16 @@ namespace WinFormsPomodoro
             // progressBar1
             // 
             this.progressBar1.ForeColor = System.Drawing.Color.DarkRed;
-            this.progressBar1.Location = new System.Drawing.Point(26, 97);
+            this.progressBar1.Location = new System.Drawing.Point(26, 45);
             this.progressBar1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.progressBar1.Name = "progressBar1";
-            this.progressBar1.Size = new System.Drawing.Size(525, 83);
+            this.progressBar1.Size = new System.Drawing.Size(525, 70);
             this.progressBar1.TabIndex = 1;
             // 
             // button1
             // 
             this.button1.BackColor = System.Drawing.Color.OliveDrab;
-            this.button1.Location = new System.Drawing.Point(26, 213);
+            this.button1.Location = new System.Drawing.Point(26, 137);
             this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(205, 70);
@@ -67,11 +73,56 @@ namespace WinFormsPomodoro
             this.button1.UseVisualStyleBackColor = false;
             this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.White;
+            this.panel1.Location = new System.Drawing.Point(304, 137);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(247, 341);
+            this.panel1.TabIndex = 3;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Red;
+            this.panel2.Location = new System.Drawing.Point(304, 463);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(247, 15);
+            this.panel2.TabIndex = 0;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Red;
+            this.button2.ForeColor = System.Drawing.Color.Khaki;
+            this.button2.Location = new System.Drawing.Point(26, 416);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(205, 62);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Запустить / Остановить";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(26, 368);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 20);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "label2";
+            // 
             // FormPomodoro
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ClientSize = new System.Drawing.Size(584, 560);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.button1);
             this.Controls.Add(this.progressBar1);
             this.Controls.Add(this.label1);
@@ -90,5 +141,10 @@ namespace WinFormsPomodoro
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
     }
 }
--- FormPomodoro.cs
+++ FormPomodoro.cs
@@ -11,9 +11,9 @@ using System.Windows.Forms;
 
 namespace WinFormsPomodoro
 {
-    // TODO  реализовать прогрессбар через таймер
     public partial class FormPomodoro : Form
     {
+        int counter;
         public FormPomodoro(string taskName)
         {
             InitializeComponent();
@@ -27,6 +27,23 @@ namespace WinFormsPomodoro
                 progressBar1.PerformStep();
                 Thread.Sleep(200);
             }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (counter % 10 == 0 )
+            {
+                label2.Text = counter < 100 ? $"00:0{counter / 10}" : $"00:{counter / 10}";
+            }
+            panel2.Top -= 1;
+            panel2.Height += 1;
+            counter++;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            timer1.Enabled = timer1.Enabled == false ? true : false;
+            counter = 0;
         }
     }
 }
